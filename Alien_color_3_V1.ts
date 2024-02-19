89d3510168874bae11c9f07c4335b1792e15d957
Tayyaba Amjad Rao <160313142+TayyabaAmjadRao@users.noreply.github.com>
2024-02-19 13:26:59 -0800

    Add files via upload

---
+++ Alien_color_3_V1.ts
@@ -0,0 +1,16 @@
+//written BY: TAYYABA RAO
+//DATE:18/FEB/2024
+
+//Version 1
+let alienColor:string ='green';
+
+if(alienColor ==="green"){
+
+    console.log("The players earned 5 points.");
+} else if(alienColor ==="yellow"){
+console.log("The players earned 10 points.");
+}else if(alienColor ==="red"){
+    console.log("The players earned 15 points.");
+}else{
+    console.log("Unknow alien color.");
+}
---
+++ Alien_color_3_V3.js
@@ -0,0 +1,47 @@
+"use strict";
+//Written by :TAYYABA RAO
+//Date: 18/feb/2024
+Object.defineProperty(exports, "__esModule", { value: true });
+//version 3
+// green alien earns 5 points
+let alien_color1 = 'green';
+if (alien_color1 === 'green') {
+    console.log("Congratulations! The player just earned 5 points for shooting the green alien.");
+}
+else if (alien_color1 === 'yellow') {
+    console.log("Congratulations! The player just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color1 === 'red') {
+    console.log("Congratulations! The player just earned 15 points for shooting the red alien.");
+}
+else {
+    console.log("Invalid alien color. Please check the color.");
+}
+//yellow alien earns 10 points
+let alien_color2 = 'yellow';
+if (alien_color2 === 'green') {
+    console.log("Congratulations! The player just earned 5 points for shooting the green alien.");
+}
+else if (alien_color2 === 'yellow') {
+    console.log("Congratulations! The player just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color2 === 'red') {
+    console.log("Congratulations! The player just earned 15 points for shooting the red alien.");
+}
+else {
+    console.log("Invalid alien color. Please check the color.");
+}
+//red alien earns 15 points
+let alien_color3 = 'red';
+if (alien_color3 === 'green') {
+    console.log("Congratulations! The player just earned 5 points for shooting the green alien.");
+}
+else if (alien_color3 === 'yellow') {
+    console.log("Congratulations! The player just earned 10 points for shooting the yellow alien.");
+}
+else if (alien_color3 === 'red') {
+    console.log("Congratulations! The player just earned 15 points for shooting the red alien.");
+}
+else {
+    console.log("Invalid alien color. Please check the color.");
+}
